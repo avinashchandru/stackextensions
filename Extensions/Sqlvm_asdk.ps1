a4a9e20048e28be5cd575b890770c8cd649bbd3a
Avinash Chandru <avich@microsoft.com>
2018-08-06 15:59:33 -0700

    edits

--- Extensions/Sqlvm_asdk.ps1
+++ Extensions/Sqlvm_asdk.ps1
@@ -1,11 +1,13 @@
+param([string]$username = "u", [string]$pwd = "p")
+
 Write-Host "Initiazlizing Sql...."
-.\initSql.ps1
+.\initSql.ps1 --u $username -p $pwd
 Write-Host "....Initiazlizing Sql done"
 
 Write-Host "Initiazlizing Doc DB...."
-.\docDbInit.ps1
+.\docDbInit.ps1 -u "asd" -p "1111"
 Write-Host "....Initiazlizing Doc DB done"
 
 Write-Host "Setting up Ibiza...."
-.\IbizaSetup.ps1
+.\IbizaSetup.ps1 -packageLocation "" -certSubjectName ""
 Write-Host "....Set up Ibiza done"
